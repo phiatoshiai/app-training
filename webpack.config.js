0959c48032aa7ec5ea7653ef58f4e50326824185
phipham.qn93 <phipham.qn93@gmail.com>
2020-07-21 14:57:15 +0700

    update code

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+
+const {resolve} = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: resolve(__dirname, 'index.js'),
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: 'app-training.js',
+        library: 'scrollSpy'
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            exclude: [/\.min\.js$/gi] // skip pre-minified libs
+        })
+    ]
+}
